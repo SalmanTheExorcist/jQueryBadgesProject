333be61b875eef422cda54ce1035ccbce4f3afbd
Salman The Exorcist Demonically Possessed <salman.the.exorcist@live.com>
2017-08-19 04:17:23 +0300

    added the JQuery required code
    updated the main JS file with the required JQuery code
    after getting our codeSchool report JSON data

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,34 @@
 $(function() {
 
-  // your code will go here
+
+      // your code will go here
+      $.ajax({
+         url: 'https://www.codeschool.com/users/salmana.json',
+         dataType: 'jsonp',
+         success: function(response) {
+           // handle response
+           //--we need to loop through: response.courses.completed Array
+           $.each(response.courses.completed, function( key, value )
+           {//--begin-Jquery_each-loop
+
+              var $myDiv = $("<div>", { "class": "course"});
+              $myDiv.append("<h3>"+ value.title  +"</h3>");
+
+              var $myImage = $("<img>",{"src": value.badge,"alt":"badge image"});
+              $myDiv.append($myImage);
+
+              var $myAnchor = $("<a>",{"href": value.url, "target":"_blank","class":"btn btn-primary"});
+              $myAnchor.append("See Course");
+              $myDiv.append($myAnchor);
+
+            $("#badges").append($myDiv);
+
+           });//--end-Jquery_each-Loop
+
+
+     }//--end--success-callback-function
+   });//--end--ajax-call
+
+
 
 });
